9f16b1956b46a3d5b2afb50be2473bcb52f8f630
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-01-25 20:49:00 +0530

    increment

--- src/context.js
+++ src/context.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { createContext, useContext, useState } from "react";
 import data from "./data";
 
@@ -6,7 +7,25 @@ const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [products, setProducts] = useState(data);
 
-  const value = { products };
+  const increment = (id) => {
+    let product = products.find((item) => item.id === id);
+    product.amount += 1;
+
+    // setProducts(products); -> This wasn't re-rendereing the component as its reference is not chandging, only it's values are changing, for which useState was not considering it as a state change, so it was not triggering re-render
+
+    // let p1 = [...products]; // here reference is changing
+    // let p2 = products;      // here no reference change, the same reference is getting copied.
+    // console.log(p1);
+    // console.log(p2);
+    // console.log(p1 === p2); // fasle as both reference are different
+
+    // setProducts(p) -> reference not changing hence state not re-rendering
+
+    setProducts([...products]);
+  };
+
+  const value = { products, increment };
+
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
